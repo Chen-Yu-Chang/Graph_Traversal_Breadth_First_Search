523f79a55d6eaacd02895d2dd65af0f8b3405a00
Chen-Yu Chang <cy890509@bu.edu>
2021-04-20 15:03:46 +0800

    add

--- bfs.cuh
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int bfsWithGpu(const unsigned int *offsets, const unsigned int *neighbours, const unsigned int vertCount, const unsigned int startVert, unsigned int *distances);
-int bfsWithCpu(const unsigned int *offsets, const unsigned int *neighbours, const unsigned int vertCount, const unsigned int startVert, unsigned int *distances);
--- testing.cuh
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void runTests(const unsigned int *offsets, const unsigned int *neighbours, const unsigned int vertCount, const unsigned int startVert, int cpuRep = 1, int gpuRep = 1);
